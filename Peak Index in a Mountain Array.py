20216a5bebe37798e355773c992a8bb1cfab984e
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-07-01 18:57:18 +0530

    Update Peak Index in a Mountain Array.py

--- Peak Index in a Mountain Array.py
+++ Peak Index in a Mountain Array.py
@@ -5,10 +5,17 @@ class Solution:
             end = len(arr) - 1
             while start < end:
                 mid = math.floor((start + (end - start) / 2))
+                # you are in dec part of array
+                # this may be the ans, but look at left
+                # this is why end != mid - 1
                 if arr[mid] > arr[mid+1]:
                     end = mid
                 else:
                     start = mid + 1
             return start
-
-       
+ # in the end, start == end and pointing to the largest number because of the 2 checks above
+ #  start and end are always trying to find max element in the above 2 checks
+ #  hence, when they are pointing to just one element, that is the max one because that is what the checks say
+ #  more elaboration: at every point of time for start and end, they have the best possible answer till that time
+ #  and if we are saying that only one item is remaining, hence cuz of above line that is the best possible ans
+ #  return start; // or return end as both are =
