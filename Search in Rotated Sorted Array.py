3ffb146cc8e24b4b08a391cb7bcf3da900612fea
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-07-08 20:13:01 +0530

    Create Search in Rotated Sorted Array.py

---
+++ Search in Rotated Sorted Array.py
@@ -0,0 +1,50 @@
+import math
+from typing import List
+
+class Solution:
+    def binary_search(self, arr: List[int], target: int, start: int, end: int) -> int:
+        while start <= end:
+            mid = math.floor(start + (end - start) / 2)
+            if target == arr[mid]:
+                return mid
+            if target < arr[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
+
+    def search(self, arr: List[int], target: int) -> int:
+        # find the pivot element
+        pivot = self.find_pivot(arr)
+        # if you didn't find out the pivot element then
+        if pivot == -1:
+            return self.binary_search(arr, target, 0, len(arr) - 1)
+        # if pivot is found you have found out  2 asc sorted arrays
+        if arr[pivot] == target:
+            return pivot
+        # if the target is greater than start
+        # search starting from 0, till pivot - 1
+        # because the elements after pivot will be smaller
+        if target >= arr[0]:
+            return self.binary_search(arr, target, 0, pivot - 1)
+        # if the target is lesser than start
+        # search starting from pivot + 1, till end
+        # because the elements after pivot will be smaller
+        # as well as we know that elements from start to pivot
+        # are going to be bigger than target
+        return self.binary_search(arr, target, pivot + 1, len(arr) - 1)
+
+    def find_pivot(self, arr: List[int]) -> int:
+        start = 0
+        end = len(arr) - 1
+        while start <= end:
+            mid = math.floor(start + (end - start) / 2)
+            if mid < end and arr[mid] > arr[mid + 1]:
+                return mid
+            if mid > start and arr[mid] < arr[mid - 1]:
+                return mid - 1
+            if arr[mid] <= arr[start]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
