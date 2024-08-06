75c4d5934daaf6d8a0019193a8fdacf1da5d4142
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-08-06 16:35:03 +0530

    Create 268. Missing Number.py

---
+++ Sorting/pythonProject/268. Missing Number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def swap(self,arr,first,second):
+        temp = arr[first]
+        arr[first] = arr[second]
+        arr[second] = temp
+
+    def missingNumber(self, arr: List[int]) -> int:
+        i = 0
+        while i < len(arr):
+            correct = arr[i]
+            # below condition checks the array out of bound case
+            if arr[i] < len(arr) and arr[i] != arr[correct]:
+                self.swap(arr,i,correct)
+            else:
+                i+=1
+        for i in range(len(arr)):
+            if arr[i] != i:
+                return i
+        # this is for the case where the number does'nt exists in the array
+        return len(arr)
