7c40a23d1b1066294aec6031eb0e2b71f78279d6
SAI-MOHAN-B <160308811+SAI-MOHAN-B@users.noreply.github.com>
2024-08-27 16:47:36 +0530

    Update multiply.py

--- recursion/multiply.py
+++ recursion/multiply.py
@@ -6,3 +6,12 @@ def digit_sum(n):
     return (n%10)*(digit_sum(n//10))
 res = digit_sum(1342)
 print(res)
+
+# sum of digits in recursion
+import math
+def digit_sum(n):
+    if n== 0:
+        return 0
+    return (n%10)+(digit_sum(n//10))
+res = digit_sum(13)
+print(res)
